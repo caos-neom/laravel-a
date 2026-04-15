96691d8af48e11787513bf683071e3c0c8f89f51
caos-neom <[miguelagustoxsilveira@gmail.com>
2026-04-15 18:27:30 -0300

    banco de dados

---
+++ resources/views/aluno/index.blade.php
@@ -0,0 +1,13 @@
+<form action="{{ route('aluno.adicionar') }}" method="post">
+    @csrf
+    <label for="nome">Nome</label>
+        <input type="text" name="nome" id="nome">
+
+    <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+   
+        <button type="submit">Salvar</button>
+        @isset($sucesso)
+        <h1>{{ $sucesso }}</h1>
+        @endisset
+</form>
